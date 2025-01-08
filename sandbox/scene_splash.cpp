c0d5c67bc68abb898b9a0691b7c3fd76c80c672a
panyi <panyi@yixin.im>
2025-01-08 19:13:43 +0800

    splash add menus

--- sandbox/scene_splash.cpp
+++ sandbox/scene_splash.cpp
@@ -1,24 +1,61 @@
 #include "scene_splash.h"
 #include "purple_ui.h"
+#include "render/sprite.h"
 
 void SceneSplash::init(){
-    
+    logoImage = purple::BuildImageByAsset("img/logo.png");
+
+    logoTop = purple::Engine::ScreenHeight - 120;
+    logoWidth = purple::Engine::ScreenHeight / 1.5f;
 }
 
 void SceneSplash::update(){
+    logoLeft = purple::Engine::ScreenWidth / 2.0f - logoWidth / 2.0f;
     
+    float ratio = static_cast<float>(logoImage->getWidth()) / static_cast<float>(logoImage->getHeight());
+    // std::cout << "ratio = " << ratio << std::endl;
+    logoHeight = logoWidth / ratio;
+    // std::cout << "logoW = " << logoWidth << "logoHeight = " << logoHeight << std::endl;
 }
 
 void SceneSplash::render(){
     using namespace purple;
 
+    Rect logoSrcRect = logoImage->getRect();
+    Rect logoDstRect(logoLeft , logoTop , logoWidth , logoHeight);    
+
+    auto spriteBatch = Engine::getRenderEngine()->getSpriteBatch();
+    spriteBatch->begin();
+    spriteBatch->renderImage(logoImage, logoSrcRect , logoDstRect);
+    spriteBatch->end();
+
     auto textRender = Engine::getRenderEngine()->getTextRender();
-    Rect rect(0.0f, purple::Engine::ScreenHeight
-        , purple::Engine::ScreenWidth,  purple::Engine::ScreenHeight);
+    
     TextPaint textPaint;
-    textPaint.setTextSize(100.0f);
-    textPaint.textColor = ConverColorValue(Color::Silver);
-    textPaint.textGravity = TextGravity::Center;
-    Engine::getRenderEngine()->renderTextWithRect(L"Terris\n俄罗斯方块", rect , textPaint , nullptr);
+    textPaint.textColor = ConverColorValue(Color::White);
+    const float textHeight = 40.0f;
+    textPaint.setTextSize(textHeight);
+
+    float menuBottomPos = logoDstRect.getBottom() - 30.0f - textHeight;
+
+    for(int i = 0, len = menuNames.size() ; i < len ;i++){
+        auto menuStr = menuNames[i];
+        Rect textOutRect;
+        Engine::getRenderEngine()->preCalculateTextSize(menuStr, textPaint, 
+            purple::Engine::ScreenWidth, textOutRect);
+        
+        float textWidth = textOutRect.width;
+        float menuLeftPos = purple::Engine::ScreenWidth / 2.0f - textWidth / 2.0f;
+        
+        if(i == currentMenuIndex){
+            textPaint.textColor = ColorTextSelected;
+        }else{
+            textPaint.textColor = ColorTextUnselected;
+        }
+        
+        Engine::getRenderEngine()->renderText(menuStr , menuLeftPos, 
+            menuBottomPos , textPaint);
+        menuBottomPos -= (20.0f + textHeight);
+    }//end for i
 }
 
--- sandbox/scene_splash.h
+++ sandbox/scene_splash.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include "purple.h"
+#include <vector>
+#include <string>
 
 class TetrisGame;
+class TextureImage;
+
 class SceneSplash{
 public:
     SceneSplash(TetrisGame *_game):game(_game){
@@ -15,4 +19,17 @@ public:
     void render();
 private:
     TetrisGame *game;
+
+    std::shared_ptr<purple::TextureImage> logoImage;
+
+    float logoTop;
+    float logoLeft;
+    float logoWidth;
+    float logoHeight;
+
+    glm::vec4 ColorTextSelected = glm::vec4(1.0f , 1.0f , 1.0f , 1.0f);
+    glm::vec4 ColorTextUnselected = glm::vec4(1.0f , 1.0f , 1.0f , 0.3f);
+
+    std::vector<std::wstring> menuNames = {L"新游戏",L"退出"};
+    int currentMenuIndex = 0;
 };
